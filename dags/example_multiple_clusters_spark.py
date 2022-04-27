20730acd594d21ee112bc33f32e61bab43fcdcb7
Daniel Pineda <danielpinedaj@gmail.com>
2022-04-27 11:29:34 -0500

    _

--- dags/example_multiple_clusters_spark.py
+++ dags/example_multiple_clusters_spark.py
@@ -1,2 +1,25 @@
- from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator 
- import pyspark
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+import pyspark
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.utcnow(),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'kubernetes_spark_submit', default_args=default_args, schedule_interval=timedelta(minutes=10))
+
+
+start = DummyOperator(task_id='run_this_first', dag=dag)
+
+
+start
