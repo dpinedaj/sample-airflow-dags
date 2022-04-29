80b6c1c66a59c510984cdd7b10eb73debbbd3afb
Daniel Pineda <danielpinedaj@gmail.com>
2022-04-29 17:00:09 -0500

    _

--- dags/plugins/hd_insight_plugin/__init__.py
+++ dags/plugins/hd_insight_plugin/__init__.py
@@ -1,9 +1,9 @@
 from airflow.plugins_manager import AirflowPlugin
 
-from .operators import HdInsightRunCmdOperator, HdInsightCreateClusterOperator, HdInsightDeleteClusterOperator
+from .operators import HDInsightRunCmdOperator, HDInsightCreateClusterOperator, HDInsightDeleteClusterOperator
 from .hooks import AzureHDInsightHook
 
 class HdInsightPlugin(AirflowPlugin):
     name = 'hd_insight_plugin'
-    operators = [HdInsightRunCmdOperator, HdInsightCreateClusterOperator, HdInsightDeleteClusterOperator]
+    operators = [HDInsightRunCmdOperator, HDInsightCreateClusterOperator, HDInsightDeleteClusterOperator]
     hooks = [AzureHDInsightHook]
